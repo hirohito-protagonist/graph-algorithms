422d8381399117b6bd311aa3922053c5bfa5231c
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-01-06 18:13:31 +0100

    feat: add input methods guards

--- test/adjacency-matrix-graph.js
+++ test/adjacency-matrix-graph.js
@@ -10,6 +10,80 @@ const AdjacencyMatrixGraph = require('./../lib/adjacency-matrix-graph');
 
 describe('AdjacencyMatrixGraph', () => {
 
+    describe('addEdge', () => {
+
+        [
+            { v1: 0, v2: -1 },
+            { v1: -1, v2: 0 },
+            { v1: 4, v2: 0 },
+            { v1: 0, v2: 4 }
+        ].forEach((testData) => {
+           
+            it(`should throw exception when vertices ${testData.v1} and ${testData.v2} are out of bounds`, (done) => {
+
+                try {
+                    const graph = new AdjacencyMatrixGraph(4);
+                    graph.addEdge(testData.v1, testData.v2);                
+                } catch(e) {
+                    expect(e.message).to.equal(`Vertices ${testData.v1} and ${testData.v2} are out of bounds`);
+                    done();
+                }            
+            });
+        });
+
+        it('should throw exception when weight is less than one', (done) => {
+
+            try {
+                const graph = new AdjacencyMatrixGraph(4);
+                graph.addEdge(0, 1, 0);                
+            } catch(e) {
+                expect(e.message).to.equal('An edge cannot have weight < 1');
+                done();
+            }
+            
+        });
+    });
+
+    describe('getAdjacentVertices', () => {
+
+        [
+            { vertex: -1 },
+            { vertex: 4 },
+        ].forEach((testData) => {
+           
+            it(`should throw exception when number of vertex ${testData.vertex} is out of range`, (done) => {
+
+                try {
+                    const graph = new AdjacencyMatrixGraph(4);
+                    graph.getAdjacentVertices(testData.vertex);                
+                } catch(e) {
+                    expect(e.message).to.equal(`Cannot access vertex ${testData.vertex}`);
+                    done();
+                }            
+            });
+        });
+    });
+
+    describe('getInDegree', () => {
+
+        [
+            { vertex: -1 },
+            { vertex: 4 },
+        ].forEach((testData) => {
+           
+            it(`should throw exception when number of vertex ${testData.vertex} is out of range`, (done) => {
+
+                try {
+                    const graph = new AdjacencyMatrixGraph(4);
+                    graph.getInDegree(testData.vertex);                
+                } catch(e) {
+                    expect(e.message).to.equal(`Cannot access vertex ${testData.vertex}`);
+                    done();
+                }            
+            });
+        });
+    });
+
     describe('Directed', () => {
 
         let graph;
