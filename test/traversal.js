77893c76ab41b7cb38cf0953fada950e8ead90d2
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-01-12 21:01:34 +0100

    test: add test traversal for directed graph

--- test/traversal.js
+++ test/traversal.js
@@ -11,53 +11,108 @@ const AdjacencyMatrixGraph = require('./../lib/adjacency-matrix-graph');
 
 describe('Traversal', () => {
         
-    let graph;
+    describe('Directed', () => {
+        let graph;
 
-    beforeEach((done) => {
-
-        /*
-         * 0_______1_______5________6_________8 
-         *         \                \
-         *          2________________3
-         *          \________________\__4
-         *          \___7
-         */
-        graph = new AdjacencyMatrixGraph(9);
-        graph.addEdge(0, 1);
-        graph.addEdge(1, 2);
-        graph.addEdge(2, 7);
-        graph.addEdge(2, 4);
-        graph.addEdge(2, 3);
-        graph.addEdge(1, 5);
-        graph.addEdge(5, 6);
-        graph.addEdge(6, 3);
-        graph.addEdge(6, 8);
-        done();
-    });
-
-    describe('breadthFirst', () => {
-
-        it('should visit graph in correct order', (done) => {
-
-            const vertices = [];
-
-            Traversal.breadthFirst(graph, 2, (vertex) => vertices.push(vertex));
-
-            expect(vertices).to.equal([2, 1, 3, 4, 7, 0, 5, 6, 8]);
+        beforeEach((done) => {
+    
+            /*
+             * 0------>1----->5------->6---------->8 
+             *          \               \
+             *           2-------------->3
+             *            \--------------------->4
+             *             \--->7
+             */
+            graph = new AdjacencyMatrixGraph(9, true);
+            graph.addEdge(0, 1);
+            graph.addEdge(1, 2);
+            graph.addEdge(2, 7);
+            graph.addEdge(2, 4);
+            graph.addEdge(2, 3);
+            graph.addEdge(1, 5);
+            graph.addEdge(5, 6);
+            graph.addEdge(6, 3);
+            graph.addEdge(6, 8);
             done();
+        });
+    
+        describe('breadthFirst', () => {
+    
+            it('should visit graph in correct order', (done) => {
+    
+                const vertices = [];
+    
+                Traversal.breadthFirst(graph, 2, (vertex) => vertices.push(vertex));
+    
+                expect(vertices).to.equal([2, 3, 4, 7]);
+                done();
+            });
+        });
+    
+        describe('depthFirst', () => {
+    
+            it('should visit graph in correct order', (done) => {
+    
+                const vertices = [];
+    
+                Traversal.depthFirst(graph, (vertex) => vertices.push(vertex));
+    
+                expect(vertices).to.equal([0, 1, 2, 3, 4, 7, 5, 6, 8]);
+                done();
+            });
         });
     });
 
-    describe('depthFirst', () => {
+    describe('Undirected', () => {
 
-        it('should visit graph in correct order', (done) => {
+        let graph;
 
-            const vertices = [];
-
-            Traversal.depthFirst(graph, (vertex) => vertices.push(vertex));
-
-            expect(vertices).to.equal([0, 1, 2, 3, 6, 5, 8, 4, 7]);
+        beforeEach((done) => {
+    
+            /*
+             * 0-------1------5-------6-----------8 
+             *          \              \
+             *           2--------------3
+             *            \----------------------4
+             *             \----7
+             */
+            graph = new AdjacencyMatrixGraph(9);
+            graph.addEdge(0, 1);
+            graph.addEdge(1, 2);
+            graph.addEdge(2, 7);
+            graph.addEdge(2, 4);
+            graph.addEdge(2, 3);
+            graph.addEdge(1, 5);
+            graph.addEdge(5, 6);
+            graph.addEdge(6, 3);
+            graph.addEdge(6, 8);
             done();
+        });
+    
+        describe('breadthFirst', () => {
+    
+            it('should visit graph in correct order', (done) => {
+    
+                const vertices = [];
+    
+                Traversal.breadthFirst(graph, 2, (vertex) => vertices.push(vertex));
+    
+                expect(vertices).to.equal([2, 1, 3, 4, 7, 0, 5, 6, 8]);
+                done();
+            });
+        });
+    
+        describe('depthFirst', () => {
+    
+            it('should visit graph in correct order', (done) => {
+    
+                const vertices = [];
+    
+                Traversal.depthFirst(graph, (vertex) => vertices.push(vertex));
+    
+                expect(vertices).to.equal([0, 1, 2, 3, 6, 5, 8, 4, 7]);
+                done();
+            });
         });
     });
 });
